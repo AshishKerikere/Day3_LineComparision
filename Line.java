595c519d41d561982ef4f6a44bb064b3e117a77d
AshishKerikere <93969829+AshishKerikere@users.noreply.github.com>
2022-11-06 18:37:39 +0530

    Updated Line Class till UC1

--- Line.java
+++ Line.java
@@ -1,1 +1,18 @@
+package Day8Assignment;
+import Day8Assignment.Point;
+public class Line{
 
+    Point p1;
+    Point p2;
+    private double lineDistance;
+
+          Line(Point p1, Point p2){
+          this.p1 = p1;
+          this.p2 = p2;
+    }
+
+    public double lineLength(){
+        lineDistance = Math.pow(Math.pow(p1.x - p2.x, 2) + Math.pow( p1.y - p2.y, 2), 0.5);
+        return lineDistance;
+    }
+}
